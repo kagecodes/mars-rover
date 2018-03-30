9b35aa853bcce2cf8f0b917473188660da9614c8
Karen <kjgamis@live.ca>
2018-03-30 16:31:45 -0400

    backbone

---
+++ marsrover.rb
@@ -0,0 +1,33 @@
+class Rover
+
+  def initialize(x_coordinate, y_coordinate, direction)
+    @x_coordinate = x_coordinate
+    @y_coordinate = y_coordinate
+    @direction
+  end
+
+  def x_coordinate
+    @x_coordinate
+  end
+
+  def y_coordinate
+    @y_coordinate
+  end
+
+  def direction
+    @direction
+  end
+
+  def read_instruction
+
+  end
+
+  def move
+
+  end
+
+  def turn
+
+  end
+
+end
